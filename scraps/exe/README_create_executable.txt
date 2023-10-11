7302410ee8e82004b05e1117c712e0ee968538f6
Julien Groenenboom <juliengroenenboom@hotmail.com>
2023-10-11 22:13:09 +0200

    Updates/fixes as ImageDraw object (PIL / Pillow) didn't have textsize attribute anymore. I was able to create a .exe from the main.py. Tests showed that it works. Now let's see if it works on their side.

--- scraps/exe/README_create_executable.txt
+++ scraps/exe/README_create_executable.txt
@@ -6,7 +6,10 @@
 
 3] Generate the .exe File
 	Run the following command to generate the .exe file:
-	pyinstaller --onefile hello_name.py
+	pyinstaller --onefile hello_name.py  (for testing)
+	pyinstaller --onefile main.py        (for actual program)
 
 4] Locate the .exe File
 	After PyInstaller finishes its process, you can find the .exe file in the dist directory within your project folder.
+
+5] Note: I got some comments/warnings about adding a line to "main.spec" and subsequently running "pyinstaller --onefile main.spec". That eventually resulted in a .exe :-)
--- utils/print_bingo_card_image.py
+++ utils/print_bingo_card_image.py
@@ -2,43 +2,49 @@ from PIL import Image, ImageDraw, ImageFont
 import tempfile
 import textwrap
 
+def get_text_size_in_pixels(font, text):
+    bbox = font.getbbox(text)
+    text_width = bbox[2] - bbox[0]
+    text_height = bbox[3] - bbox[1]
+    return text_width, text_height
+
+def print_to_card(draw, x, y, text, font, up_or_down, wrap_width=15):
+    text_wrapped = textwrap.wrap(text, width=wrap_width)
+    if (len(text_wrapped) > 1) and up_or_down == 'up':
+        text_wrapped = reversed(text_wrapped) # work from center up or down, so reverse to ['Kids','War','Cold']
+    
+    delta_y = 22
+    for line in text_wrapped:
+        if up_or_down == 'up':
+            y = int(y - delta_y)
+        else:
+            y = int(y + delta_y)
+        draw.text((x, y), line, fill="black", anchor="mm", font=font)
+        delta_y += 8 # _, line_height = get_text_size_in_pixels(font, 'Height of a line with random text') 
+
 def make_bingo_card(image_path, df, card_size=9):
     img = Image.open(image_path)
     draw = ImageDraw.Draw(img)
     font = ImageFont.truetype("cour.ttf", 20)
 
-    line_width,line_height = draw.textsize('-', font)
     x_center = [170, 450, 720] # center-coordinate (x-axis) of each columnn
     y_center = [290, 525, 751] # center-coordinate (y-axis) of each row
-    char_max = 14 # max nr of characters
-
+    
     num_rows_cols = int(len(df) ** 0.5)
     for row in range(num_rows_cols):
         for col in range(num_rows_cols):
             index = row*3+col
             
-            x_cen = x_center[col]
-            y_cen = y_center[row]
-            
-            # print artist
-            artist_wrapped = textwrap.fill(df['artist'][index], width=char_max)
-            text_width, text_height = draw.textsize(artist_wrapped, font)
-            x = x_cen - text_width/2
-            y = y_cen - 1.3*line_height - text_height
-            draw.text((x, y), artist_wrapped, fill="black", font=font)
+            x = x_center[col]
+            y = y_center[row]
             
             # print '-'
-            x = x_cen
-            y = y_cen
-            draw.text((x, y), '-', fill="black", font=font)
+            draw.text((x, y), '-', fill="black", anchor="mm", font=font)
             
-            # print title
-            title_wrapped = textwrap.fill(df['title'][index], width=char_max)
-            text_width, text_height = draw.textsize(title_wrapped, font)
-            x = x_cen - text_width/2
-            y = y_cen + 1.3*line_height
-            draw.text((x, y), title_wrapped, fill="black", font=font)
-            
+            # print artist and title
+            print_to_card(draw, x, y, df['artist'][index], font, up_or_down='up')
+            print_to_card(draw, x, y, df['title'][index], font, up_or_down='down')
+
     img_rotated = img.transpose(Image.ROTATE_90)
 
     # Save the modified image to a temporary file
